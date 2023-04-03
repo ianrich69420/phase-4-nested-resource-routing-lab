6daa5d5be7c223562c726366d39cd6765907dc5b
ian <ian.orieko@student.moringaschool.com>
2023-04-03 07:06:06 +0300

    Completed assignment

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,36 @@
 class ItemsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   def index
-    items = Item.all
-    render json: items, include: :user
+    if !params[:user_id]
+      render json: Item.all, except: [:created_at, :updated_at], include: :user
+    else
+      render json: find_user.items, except: [:created_at, :updated_at]
+    end
   end
 
+  def show
+    render json: Item.find(params[:id]), except: [:created_at, :updated_at]
+  end
+
+  def create
+    user = find_user
+    item = Item.create(item_params)
+    user.items << item
+    render json: item, except: [:created_at, :updated_at], status: :created
+  end
+
+  private
+
+  def find_user
+    User.find(params[:user_id])
+  end
+
+  def item_params
+    params.permit(:name, :description, :price)
+  end
+
+  def render_not_found_response
+    render json: { error: "User not found" }, status: :not_found
+  end  
 end
